8427e24503cb405e77ee6abb096bb6e2582f1ec4
George Everitt <geveritt@appliedrelevance.com>
2023-08-26 18:28:33 +0000

    WIP - Added Initial doctypes

---
+++ cotc/cult_of_the_carrot/doctype/ingredient/ingredient.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Geordie Everitt and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Ingredient', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ cotc/cult_of_the_carrot/doctype/recipe/recipe.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Geordie Everitt and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Recipe', {
+	// refresh: function(frm) {
+
+	// }
+});
