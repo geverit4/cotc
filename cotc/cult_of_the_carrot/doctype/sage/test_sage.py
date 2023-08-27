4209e2df7a3899cc6a207a7b94fa26f1bf6abd37
George Everitt <geveritt@appliedrelevance.com>
2023-08-27 00:13:08 +0000

    WIP - Interactive Development

---
+++ cotc/cult_of_the_carrot/doctype/sage/test_sage.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Geordie Everitt and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSage(FrappeTestCase):
+	pass
