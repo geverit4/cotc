a8ad68c920f385de9b9906b694985c082486e3ec
George Everitt <geveritt@appliedrelevance.com>
2023-11-18 17:21:47 +0000

    Change module name

--- cotc/config/desktop.py
+++ cotc/config/desktop.py
@@ -3,7 +3,7 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Cult of the Carrot",
+			"module_name": "cotc",
 			"type": "module",
 			"label": _("Cult of the Carrot")
 		}
